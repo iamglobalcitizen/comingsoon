3169d30172fd753cd0e12078291fc53ac54d764b
itgloballearner <it@globallearner.com.au>
2019-10-02 21:17:34 +1000

    adjusted countdown units

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,7 +181,7 @@ var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
 var seconds = Math.floor((t % (1000 * 60)) / 1000); 
 document.getElementById("countdown").innerHTML = days + " days &nbsp; | &nbsp; "  
-+ hours + " hours &nbsp; | &nbsp; " + minutes + " minutes &nbsp; | &nbsp; " + seconds + " seconds "; 
++ hours + " hrs &nbsp; | &nbsp; " + minutes + " mins &nbsp; | &nbsp; " + seconds + " s "; 
     if (t < 0) { 
         clearInterval(x); 
         document.getElementById("countdown").innerHTML = "Visit https://www.iamglobalcitizen.com.au"; 
